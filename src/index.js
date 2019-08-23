72d49255a72c2902a2119c98c3b25c335ad913d3
Raketchiza8 <raketchiza@mail.ru>
2019-08-23 15:15:17 +0400

    Homework lesson 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
+let money       = 40000, 
+    income      = 'freelanser', 
+    addExpenses = 'shooping, study, car', 
+    deposit     = false, 
+    mission     = 500000, 
+    period      = 8;
 
-alert('Hello,world!');
-console.log('Привет,мир!');
+console.log(typeof money); 
+console.log(typeof income);   
+console.log(typeof deposit);
+
+console.log(income.length);
+
+console.log('Период ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
+console.log(money % 30);
