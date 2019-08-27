4f02cb2a43853388811af4b771001632cd3e3ddf
Raketchiza8 <raketchiza@mail.ru>
2019-08-27 18:15:22 +0400

    Homwork lesson3

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
-let money       = +prompt('Ваш ежемесячный доход?'), 
+let money       = +prompt('Ваш ежемесячный доход?', 20000),
     income      = 'freelanser', 
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'), 
     deposit     = confirm('Есть ли у вас депозит в банке?'), 
-    mission     = 500000, 
+    mission     = 500000,
     period      = 8;
 
 // 2)
 console.log(addExpenses.split(','));
 
-// 4)
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-
 // 5) 
 let charges  = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    amount   = +prompt('Во сколько это обойдется?');
+    amount   = +prompt('Во сколько это обойдется?',3000);
     charges2 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    amount2  = +prompt('Во сколько это обойдется?');
+    amount2  = +prompt('Во сколько это обойдется?', 10000);
 
 // 6)
 let budgetMonth = money - amount - amount2;
@@ -28,23 +23,62 @@ console.log( Math.ceil(mission / budgetMonth) );
 
 // 8)
 let budgetDay = Math.floor(budgetMonth / 31);
-console.log( budgetDay);
+console.log('budgetDay: ', budgetDay);
 
-// 9)
-if(budgetDay > 0) {
 
-    if (budgetDay >= 800) {
-        console.log('Высокий уровень дохода');
-    } else if (300 <= budgetDay < 800) {
-        console.log('Средний уровень дохода');
-    } else {
-        console.log('Низкий уровень дохода');
-    }
-} else {
-    console.log('Что то пошло не так');
+
+
+
+
+//!!!!!!!!!!!!!!!!!  lesson 4 !!!!!!!!!!!!!!!!!!!
+
+// 1)
+let getExpensesMonth = function() {
+    return amount + amount2;
+};
+getExpensesMonth();
+
+
+let getAccumulatedMonth = function(money, getAmounts) {
+    return money - getAmounts(); 
+};
+
+
+let accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth);
+console.log('accumulatedMonth: ', accumulatedMonth);
+
+
+
+let getTargetMonth = function(mission, accumulatedMonth) {
+    return Math.ceil( mission / accumulatedMonth);
 }
+getTargetMonth(mission, accumulatedMonth);
 
+// 2)
 
+let showTypeOf = function(data) {
+     console.log(data, typeof(data));
+ }
 
+ showTypeOf(money);
+ showTypeOf(income);
+ showTypeOf(deposit);
+
+let getStatusIncome = function() {
+    if(budgetDay > 0) {
+
+        if (budgetDay < 300) {
+            return 'Низкий уровень дохода';
+        } else if (budgetDay <= 800) {
+            return 'Средний уровень дохода';
+        } else {
+            return 'Высокий уровень дохода';
+        }
+
+    } else {
+        return 'Что то пошло не так';
+    }
+}
+getStatusIncome();
 
 
