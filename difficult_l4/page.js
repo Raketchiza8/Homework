54c55bed320d5e876dc3612292de720ebca12c32
Raketchiza8 <raketchiza@mail.ru>
2019-08-28 15:32:24 +0400

    syntax error fixed

--- difficult_l4/page.js
+++ difficult_l4/page.js
@@ -1,13 +1,11 @@
 //1)
 let actionWithText = function(text) {
     if(typeof text !== 'string') {
-        return 'Введнное значение не является строкой'
+        return 'Введнное значение не является строкой';
     } else {
         let newText = String(new RegExp(text.trim()));
         newText = newText.slice(1, newText.length -1);
         
-
-       
         if(newText.length > 30) {
             return newText.slice(0, 31) + '...';
         } else {
