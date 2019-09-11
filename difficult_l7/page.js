6d3eb25bb92e9cf342ac52cb0fdadb1ed2372d11
Raketchiza8 <raketchiza@mail.ru>
2019-09-12 01:26:45 +0400

    Homwork lesson10 task 1

--- difficult_l7/page.js
+++ difficult_l7/page.js
@@ -1,22 +1,31 @@
-let date  = new Date(),
-currentDate = {
-    hour:date.getHours(),
-    minute:date.getMinutes(),
-    second:date.getSeconds(),
-    day:date.getDate(),
-    month:date.getMonth(),
-    year:date.getFullYear()
-}
-document.write( currentDate.hour + ':' + currentDate.minute + ':' + currentDate.second + ' ' + currentDate.day + '.' + currentDate.month + '.' + currentDate.year );    
-
-let addZero = function(obj) {
-  for(let key in obj) {
-    obj[key] = String(obj[key]);
-    if(obj[key].length === 1) {
-      obj[key] = '0' + obj[key];
-    }
-  }
+const DomElement = function(selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
 };
-addZero(currentDate);
-document.write( '<br/>' + currentDate.hour + ':' + currentDate.minute + ':' + currentDate.second + ' ' + currentDate.day + '.' + currentDate.month + '.' + currentDate.year );    
 
+DomElement.prototype.createElement = function() {
+  let newElement;
+  if(this.selector[0] === '.') {
+    newElement = document.createElement('div');
+    newElement.classList.add(this.selector.slice(1));
+    newElement.textContent = 'Квадрат';
+  } 
+  else if(this.selector[0] === '#') {
+    newElement = document.createElement('p');
+    newElement.textContent = 'Параграф с текстом';
+  }
+  newElement.style.cssText = `
+    height: ${this.height};
+    width: ${this.width};
+    background-color: ${this.bg};
+    font-size: ${this.fontSize};`;
+    document.body.appendChild(newElement);
+};
+
+const newDom1 = new DomElement('.squer', '150px', '150px', 'red', '20px');
+const newDom2 = new DomElement('#parag', '40px', '150px', 'green', '15px');
+newDom1.createElement();
+newDom2.createElement();
